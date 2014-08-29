56ae3e50ee849057605533c437dae57e96419a37
phil.roche <phil.roche@ticket-text.com>
2014-08-29 09:42:19 +0100

    Patched version of nvd3 to use our own version of it's static files. IT was pointing to ones that no longer existed

---
+++ demoproject/demoproject/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import patterns, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('demoproject.views',
+    # Examples:
+    url(r'^$', 'home', name='home'),
+    url(r'^piechart/', 'demo_piechart', name='demo_piechart'),
+    url(r'^linechart/', 'demo_linechart', name='demo_linechart'),
+    url(r'^linechart_without_date/', 'demo_linechart_without_date', name='demo_linechart_without_date'),
+    url(r'^linewithfocuschart/', 'demo_linewithfocuschart', name='demo_linewithfocuschart'),
+    url(r'^multibarchart/', 'demo_multibarchart', name='demo_multibarchart'),
+    url(r'^stackedareachart/', 'demo_stackedareachart', name='demo_stackedareachart'),
+    url(r'^multibarhorizontalchart/', 'demo_multibarhorizontalchart', name='demo_multibarhorizontalchart'),
+    url(r'^lineplusbarchart/', 'demo_lineplusbarchart', name='demo_lineplusbarchart'),
+    url(r'^cumulativelinechart/', 'demo_cumulativelinechart', name='demo_cumulativelinechart'),
+    url(r'^discretebarchart/', 'demo_discretebarchart', name='demo_discretebarchart'),
+    url(r'^scatterchart/', 'demo_scatterchart', name='demo_scatterchart'),
+    url(r'^linechart_with_ampm/', 'demo_linechart_with_ampm', name='demo_linechart_with_ampm'),
+    # url(r'^demoproject/', include('demoproject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import django_nvd3
+import os
+import re
+
+
+def read(*parts):
+    return open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'(\s*git)|(\s*hg)', line):
+            pass
+        else:
+            requirements.append(line)
+    return requirements
+
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+
+    return dependency_links
+
+
+setup(
+    name='django-nvd3',
+    version=django_nvd3.__version__,
+    description="Django NVD3",
+    long_description=read('README.rst'),
+    keywords='django, nvd3, chart, graph, d3',
+    url='http://github.com/areski/django-nvd3',
+    author='Belaid Arezqui',
+    author_email='areski@gmail.com',
+    license='MIT License',
+    zip_safe=False,
+    packages=find_packages(exclude=["tests", "demoproject", "docs"]),
+    include_package_data=True,
+    package_data={},
+    install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
